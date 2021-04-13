c1a9d8938f7c712452010bce0a0f4404f8e9e0af
AntoniPizarro <apizarro@cifpfbmoll.eu>
2021-04-13 16:34:38 +0200

    feat: JS rutas get
    Se ha implementado lógica al JavaScript para acceder a las rutas GET

--- client_site/functions.js
+++ client_site/functions.js
@@ -39,38 +39,98 @@ for(let item in data) {
 */
 
 function getItems() {
-    fetch('http://81.32.78.19:5505/items')
+    fetch('http://127.0.0.1:5505/items')
         .then(response => response.json())
         .then(data => {
-            data.items.forEach(book => {
-                console.log(book);
+            data.items.forEach(item => {
+                console.log(item);
                 let inventory = document.getElementById("inventory");
                 let itemBlock = document.createElement("div");
                 let itemName = document.createElement("p");
                 let itemSellIn = document.createElement("p");
                 let itemQuality = document.createElement("p");
                 itemBlock.className = "item-block";
-                itemName.innerHTML = "Name: " + book["name"];
-                itemSellIn.innerHTML = "Sell in: " + book["sell_in"];
-                itemQuality.innerHTML = "Quality: " + book["quality"];
+                itemName.innerHTML = "Name: " + item["name"];
+                itemSellIn.innerHTML = "Sell in: " + item["sell_in"];
+                itemQuality.innerHTML = "Quality: " + item["quality"];
                 itemBlock.appendChild(itemName);
                 itemBlock.appendChild(itemSellIn);
                 itemBlock.appendChild(itemQuality);
                 inventory.appendChild(itemBlock);
             })
-      });
+        });
 }
 
 function getItem(name) {
-
+    let item = name;
+    fetch('http://127.0.0.1:5505/items/' + item)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach(item => {
+                console.log(item);
+                let inventory = document.getElementById("inventory");
+                let itemBlock = document.createElement("div");
+                let itemName = document.createElement("p");
+                let itemSellIn = document.createElement("p");
+                let itemQuality = document.createElement("p");
+                itemBlock.className = "item-block";
+                itemName.innerHTML = "Name: " + item["name"];
+                itemSellIn.innerHTML = "Sell in: " + item["sell_in"];
+                itemQuality.innerHTML = "Quality: " + item["quality"];
+                itemBlock.appendChild(itemName);
+                itemBlock.appendChild(itemSellIn);
+                itemBlock.appendChild(itemQuality);
+                inventory.appendChild(itemBlock);
+            })
+        });
 }
 
 function getItemBySellIn(sell_in) {
-
+    let item = sell_in;
+    fetch('http://127.0.0.1:5505/items/sell_in/' + item)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach(item => {
+                console.log(item);
+                let inventory = document.getElementById("inventory");
+                let itemBlock = document.createElement("div");
+                let itemName = document.createElement("p");
+                let itemSellIn = document.createElement("p");
+                let itemQuality = document.createElement("p");
+                itemBlock.className = "item-block";
+                itemName.innerHTML = "Name: " + item["name"];
+                itemSellIn.innerHTML = "Sell in: " + item["sell_in"];
+                itemQuality.innerHTML = "Quality: " + item["quality"];
+                itemBlock.appendChild(itemName);
+                itemBlock.appendChild(itemSellIn);
+                itemBlock.appendChild(itemQuality);
+                inventory.appendChild(itemBlock);
+            })
+        });
 }
 
-function getItemByQuality(sell_in) {
-
+function getItemByQuality(quaity) {
+    let item = quaity;
+    fetch('http://127.0.0.1:5505/items/quality/' + item)
+        .then(response => response.json())
+        .then(data => {
+            data.items.forEach(item => {
+                console.log(item);
+                let inventory = document.getElementById("inventory");
+                let itemBlock = document.createElement("div");
+                let itemName = document.createElement("p");
+                let itemSellIn = document.createElement("p");
+                let itemQuality = document.createElement("p");
+                itemBlock.className = "item-block";
+                itemName.innerHTML = "Name: " + item["name"];
+                itemSellIn.innerHTML = "Sell in: " + item["sell_in"];
+                itemQuality.innerHTML = "Quality: " + item["quality"];
+                itemBlock.appendChild(itemName);
+                itemBlock.appendChild(itemSellIn);
+                itemBlock.appendChild(itemQuality);
+                inventory.appendChild(itemBlock);
+            })
+        });
 }
 
 function update_quality() {
